45ae9089c134a95e7654edb24ebef299e61c9931
Chandu-Thota <46589949+Chandu-Thota@users.noreply.github.com>
2025-03-08 08:44:17 +0530

    vercel integration check

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 export default function Home() {
 	return (
 		<div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
-			Hello Developers!!!
+			Hello Developers!!! 
 		</div>
+
+<div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
+Vercel is integrated !!!!!!!! 
+</div>
 	);
 }
