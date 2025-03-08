43858a3a050832b7d2835afca974a14c3fda1e0d
cyamsani3 <chandanyamsani007@gmail.com>
2025-03-08 23:57:23 +0530

    added contact us page

--- app/blogs/[blog]/page.tsx
+++ app/blogs/[blog]/page.tsx
@@ -1,4 +1,6 @@
 import { notFound } from "next/navigation";
+import { FaArrowLeft } from "react-icons/fa";
+import styles from "./page.module.css";
 
 interface Blog {
 	id: string;
@@ -24,6 +26,9 @@ export default async function BlogPage({ params }: { params: Params }) {
 
 	return (
 		<main className='container mx-auto py-10 px-6'>
+			<div>
+				<a href='/blogs' className='text-blue-500 hover:underline'><FaArrowLeft className={styles.arrowIcon} /></a>
+			</div>
 			<h1 className='text-3xl font-bold mb-4'>{blog?.title}</h1>
 			<p className='text-gray-700'>{blog?.content}</p>
 		</main>
