35b33ba7daa2721a705e8d65b09c403f8902933e
cyamsani3 <chandanyamsani007@gmail.com>
2025-03-09 11:15:27 +0530

    code refactoring

--- app/blogs/[blog]/page.tsx
+++ app/blogs/[blog]/page.tsx
@@ -1,6 +1,7 @@
 import { notFound } from "next/navigation";
 import { FaArrowLeft } from "react-icons/fa";
 import styles from "./page.module.css";
+import Link from "next/link";
 
 interface Blog {
 	id: string;
@@ -27,7 +28,7 @@ export default async function BlogPage({ params }: { params: Params }) {
 	return (
 		<main className='container mx-auto py-10 px-6'>
 			<div>
-				<a href='/blogs' className='text-blue-500 hover:underline'><FaArrowLeft className={styles.arrowIcon} /></a>
+				<Link href='/blogs' className='text-blue-500 hover:underline'><FaArrowLeft className={styles.arrowIcon} /></Link>
 			</div>
 			<h1 className='text-3xl font-bold mb-4'>{blog?.title}</h1>
 			<p className='text-gray-700'>{blog?.content}</p>
